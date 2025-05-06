0e6234813797c1b3a59ab2e8fa02bdd0e5b23817
teja463 <teja463@gmail.com>
2025-05-06 15:33:17 +0530

    Added rabbitmq hello world example

---
+++ rabbitmq-helloworld/src/main/java/com/teja/practice/Sender.java
@@ -0,0 +1,30 @@
+package com.teja.practice;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.Random;
+import java.util.concurrent.TimeoutException;
+
+public class Sender {
+
+    private final static String QUEUE_NAME = "hello";
+
+    public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Random random = new Random();
+        try(Connection connection = factory.newConnection(); Channel channel = connection.createChannel()){
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            for(int i = 0; i< 10; i++){
+                String message = "Hello World!!!" + random.nextInt(100);
+                // simulating user wait
+                Thread.sleep(1000);
+                channel.basicPublish("", QUEUE_NAME, null, message.getBytes());
+                System.out.println("[x] Sent '"+message+"'");
+            }
+        }
+    }
+}
