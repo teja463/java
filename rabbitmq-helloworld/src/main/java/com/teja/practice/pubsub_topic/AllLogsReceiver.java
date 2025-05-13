26f39a4dd292ee8e65b32c19e443ed097345b8a9
teja463 <teja463@gmail.com>
2025-05-13 17:15:23 +0530

    added examples on topic exchange

--- rabbitmq-helloworld/src/main/java/com/teja/practice/pubsub_topic/AllLogsReceiver.java
+++ rabbitmq-helloworld/src/main/java/com/teja/practice/pubsub_topic/AllLogsReceiver.java
@@ -5,7 +5,7 @@ import com.rabbitmq.client.*;
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
-public class InfoReceiver {
+public class AllLogsReceiver {
     public static void main(String[] args) throws IOException, TimeoutException {
         String EXCHANGE_NAME = "topic_logs";
         ConnectionFactory factory = new ConnectionFactory();
@@ -17,7 +17,7 @@ public class InfoReceiver {
         channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.TOPIC);
 
         String queue = channel.queueDeclare().getQueue();
-        channel.queueBind(queue,EXCHANGE_NAME, "*.info");
+        channel.queueBind(queue,EXCHANGE_NAME, "#");
         DeliverCallback callback = (consumerTag, delivery) -> {
             String message = new String(delivery.getBody());
             System.out.println("Received "+message+" on route "+delivery.getEnvelope().getRoutingKey());
--- rabbitmq-helloworld/src/main/java/com/teja/practice/pubsub_topic/Publisher.java
+++ rabbitmq-helloworld/src/main/java/com/teja/practice/pubsub_topic/Publisher.java
@@ -11,21 +11,21 @@ import java.util.concurrent.TimeoutException;
 
 public class Publisher {
     public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
-        String EXCHANGE_NAME = "direct_logs";
+        String EXCHANGE_NAME = "topic_logs";
         ConnectionFactory factory = new ConnectionFactory();
-        String[] levels = new String[]{"info", "warn", "error"};
+        String[] topics = new String[]{"anonymous.info","anonymous.critical","kern.info" ,"kern.critical"};
         factory.setHost("localhost");
         try (Connection connection = factory.newConnection(); Channel channel = connection.createChannel()) {
 
-            channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
+            channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.TOPIC);
             Random random = new Random();
             random.nextInt();
             for (int i = 1; i <= 100; i++) {
-                String logLevel = levels[random.nextInt(0, 3)];
-                String message = "Log " + i + " :" + logLevel;
+                String routeKey = topics[random.nextInt(0, 4)];
+                String message = "Hello  " + i + " :" + routeKey;
                 System.out.println("Sent " + message);
                 Thread.sleep(500);
-                channel.basicPublish(EXCHANGE_NAME, logLevel, null, message.getBytes());
+                channel.basicPublish(EXCHANGE_NAME, routeKey, null, message.getBytes());
             }
 
         } finally {
